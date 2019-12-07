f4a0c9f5aeb87d8188eb6b9d75acc8632ab4b2b1
luciamata5 <mat18013@byui.edu>
2019-12-07 14:52:14 -0700

    update

--- public/recipeClientside.js
+++ public/recipeClientside.js
@@ -8,15 +8,11 @@ function searchRecipe() {
 		console.log("Back from the server with:");
         console.log(data);
         
-        $("#ulrecipe").append("<li>" + data.ingredient_name + "</li>");
-
-        /*
 		for (var i = 0; i < data.list.length; i++) {
 			var recipe = data.list[i];
 
 			$("#ulrecipe").append("<li>" + recipe.ingredient_name + "</li>");
-		}*/
-
+		}
 	})
 }
 

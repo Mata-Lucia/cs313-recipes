4e6039ccaba55c7b6e6b109a76c43f52d0cd0e74
luciamata5 <mat18013@byui.edu>
2019-12-15 14:48:46 -0700

    update

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ function getRecipes(request, response) {
 
     getRecipeFromDB(id, function(error, result) {
         if (error || result == null /*|| result.length != 1*/) {
-			response.status(500).json({success: false, data: error});
+            response.status(500).json({success: false, data: error});
+            alert('Please enter Mac and Cheese');
 		} else {
 			//const recipe = result[0];
             //response.status(200).json(recipe);
